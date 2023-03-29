c0cbe744a8e58cd374a5beae15f5a9259f07e863
CharlieAntoni <110430728+AntonCharlie@users.noreply.github.com>
2023-03-29 17:08:09 +0700

    Add files via upload

---
+++ 02_PYGAME/00_BASIC/00_getting_started.py
@@ -0,0 +1,42 @@
+import pygame
+import sys
+
+
+class Screen:
+
+	width = 500
+	height = 500
+
+	bg_color = (0, 255, 0)
+
+	screen = pygame.display.set_mode((width, height)) # CANVAS SIZE TUPLE(WIDTH X HEIGHT)
+
+	@classmethod
+	def show(self):
+		self.screen.fill(self.bg_color)
+
+class Game:
+	pygame.init()
+
+
+	#MAINLOOP
+	@classmethod
+	def run(self):
+		while True:
+			Screen.show()
+			pygame.display.flip()
+			pygame.time.Clock().tick(25) #FPS SETTINGS
+
+			self.add_event_listener()
+
+	@classmethod
+	def add_event_listener(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+
+if __name__ == '__main__':
+	Game.run()
+
+	
+	
